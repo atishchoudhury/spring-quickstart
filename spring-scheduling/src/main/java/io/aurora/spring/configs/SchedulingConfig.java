9b535d53d55350fb01a342bec4715fe4efe4a3cb
Atish <achoudhury@vmware.com>
2019-07-31 00:05:22 +0530

    EnableScheduling annottion

--- spring-scheduling/src/main/java/io/aurora/spring/configs/SchedulingConfig.java
+++ spring-scheduling/src/main/java/io/aurora/spring/configs/SchedulingConfig.java
@@ -11,12 +11,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.Trigger;
 import org.springframework.scheduling.TriggerContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 import io.aurora.spring.utils.DynamicValueProvider;
 
 @Configuration
+@EnableScheduling
 public class SchedulingConfig implements SchedulingConfigurer{
 
 	private static final Logger log = LoggerFactory.getLogger(SchedulingConfig.class);
